7065e85e23b2e9180c19846abb468ddebcb4a813
sharriw <47089201@qq.com>
2018-01-01 22:55:04 +0800

    年后

---
+++ 456.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kitty
+ * Date: 2017-12-30
+ * Time: 17:00
+ */
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<p>
+    Hello@
+</p>
+<form action="789.php" method="post">
+    <input type="text" name="username">
+    <input type="password" name="password">
+    <input type="submit" value="查看">
+
+</form>
+
+</body>
+</html>
